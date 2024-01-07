cb323b9679491680079396e474369c0367e987ce
NXTMax <maxenceP25@gmail.com>
2024-01-07 18:19:54 +0100

    Change Product[] into List<Product>

--- P2FixAnAppDotNetCode/Models/Repositories/ProductRepository.cs
+++ P2FixAnAppDotNetCode/Models/Repositories/ProductRepository.cs
@@ -32,11 +32,7 @@ namespace P2FixAnAppDotNetCode.Models.Repositories
         /// <summary>
         /// Get all products from the inventory
         /// </summary>
-        public Product[] GetAllProducts()
-        {
-            List<Product> list = _products.Where(p => p.Stock > 0).OrderBy(p => p.Name).ToList();
-            return list.ToArray();
-        }
+        public List<Product> GetAllProducts() => _products.Where(p => p.Stock > 0).OrderBy(p => p.Name).ToList();
 
         /// <summary>
         /// Update the stock of a product in the inventory by its id
